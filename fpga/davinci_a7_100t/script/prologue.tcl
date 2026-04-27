863a2ad3c6f81c09607382d844cef37ef9a76902
Justin-Ju-0413 <245745387+Justin-Ju-0413@users.noreply.github.com>
2026-04-28 02:34:55 +0800

    fpga: stabilize davinci source management

--- fpga/davinci_a7_100t/script/prologue.tcl
+++ fpga/davinci_a7_100t/script/prologue.tcl
@@ -2,15 +2,16 @@ set origin_dir [pwd]
 set basedir $::env(BASEDIR)
 set scriptdir [file normalize [file dirname [info script]]]
 source [file join $scriptdir board.tcl]
-set srcfiles $::env(VSRCS)
+set srcfiles [split $::env(VSRCS) "\n"]
 set srcdir [file normalize [file join $basedir install rtl]]
 set ipdir [file normalize [file join $origin_dir .ip_user_files]]
 file mkdir obj
 create_project -force $name $origin_dir/obj -part $part_fpga
+set_property SOURCE_MGMT_MODE None [current_project]
 set_property top system [current_fileset]
-add_files -norecurse {*}$srcfiles
+add_files -norecurse $srcfiles
 if {[info exists ::env(EXTRA_VSRCS)] && $::env(EXTRA_VSRCS) ne ""} {
-  set extra_srcfiles $::env(EXTRA_VSRCS)
-  add_files -norecurse {*}$extra_srcfiles
+  set extra_srcfiles [split $::env(EXTRA_VSRCS) "\n"]
+  add_files -norecurse $extra_srcfiles
 }
 add_files -fileset constrs_1 [glob -nocomplain [file join $origin_dir constrs *.xdc]]
